0cc225545d592d7d0efdfdc23003da8f399b3a75
Gökser <48176173+gokserpirik@users.noreply.github.com>
2023-11-29 22:59:00 +0300

    lastlecture

--- Course/HTML-CSS/twitterish/README.md
+++ Course/HTML-CSS/twitterish/README.md
@@ -15,3 +15,20 @@ Projenin HTML/CSS dosyalarını app klasöründe global.css ve page.js içinde b
 Kullanılan görseller public klasörü içerisinde yer alıyor.
 
 - Sidenav (yan menü) oluşturuldu. CSS kısımları yapıldı.
+
+### 4-5. Hafta
+
+- Javascript ve gerekli özellikleri anlatıldı.
+
+### 6. Hafta
+
+- Import ve export işlemleri anlatıldı. Export sonraki derse ertelendi.
+- Package seçiminde önemli noktalar anlatıldı.
+- Örnek bir package kullanıldı. Icon import edildi.
+- Async ve await kullanımı anlatıldı.
+- Fetch işlemleri yapıldı.
+- Olası undefined hatalarına karşı önlem şekilleri gösterildi.
+- Verilerin class içerisinde tutulması sağlandı.
+- Oluşturulan verinin state içerisinde tutulması sağlandı.
+
+
--- Course/HTML-CSS/twitterish/app/page.js
+++ Course/HTML-CSS/twitterish/app/page.js
@@ -1,5 +1,66 @@
+/* useState ve useEffect client-side işlemler.
+bu yüzden "use client" olarak belirtiyoruz. */
+"use client";
+
+import { useState, useEffect } from 'react';
+
+/* react-icons package'ı üzerinden MdHome component'i çağırıyoruz. */
+import { MdHome } from "react-icons/md";
+
+
+
+/* User class'ı oluşturarak benzer data'mızı hem daha düzenli yapabiliyor
+hem de gerekli işlemleri bütün benzer dataları istediğimiz işlemlerden geçirebiliyoruz. */
+class User{
+  get name(){
+    return this._name;
+  }
+  get username(){
+    return this._username;
+  }
+  get profileImage(){
+    return this._profileImage;
+  }
+
+  constructor(name, username, profileImage){
+    /* this._name yerine this.name yazarsak sadece getter tanımladığımıza
+    yönelik bir hata alacağız. burada çözüm this._name kullanmak. */
+    this._name = name;
+    this._username = '@' + username;
+    this._profileImage = profileImage;
+  }
+}
 
 export default function Home() {
+
+  /* oluşabilecek herhangi bir "undefined" hatasını önlemek için geçici data ekliyoruz. */
+  const [user, setUser] = useState(new User('heyy', 'hey', 'hey'));
+  
+  /* loading, data'yı çekene kadarki kısımda bize yardımcı olacak. */
+  const [loading, setLoading] = useState(true);
+
+  /* asenkron olarak, aynı anda değil sırayla, işleyecek fonksiyonumuz kullanıcı verisini alıp User class'ı olarak user'a atayacak. */
+  async function fetchUser() {
+    
+    const res = await fetch('https://gdsctwitterauth-6yuzbc3zha-uc.a.run.app/?username=test1&password=test2')
+    const json = await res.json()
+    /* json'umuz bize başarılı durumlarda status dönüyor. 200 başarılı işlem demek.
+    diğer durumlar için placeholder bir data tanımladık. */
+    if (json.status === 200) {
+      setUser(new User(json.user.name, json.user.username, json.user.photo))
+    } else{
+      setUser(new User('Jane Doe', 'janedoe', '/profile.jpeg'))
+    }
+    setLoading(false);
+  }
+
+  /* sayfa yüklendiğinde fonksiyonumuz çalışacak */
+  useEffect(() => {
+    fetchUser();
+  }
+  , []);
+
+
   return (
       <div className="container">
         {/* tüm elementleri kontrol edebilmek için wrapper kullandık */}
@@ -14,7 +75,8 @@ export default function Home() {
               {/* görsel isimlerini bilerek karmaşık tuttuk. tek tek yazmak yerine 
               sol panelden görseli ctrl+c yapıp src kısmına yapıştırın
               baştaki public kısmını silin(bu kısmı sonra anlatacağız.) */}
-              <img src="/png/home_FILL0_wght400_GRAD0_opsz24.png" alt="" />
+             
+              <MdHome /> {/* burada görsel kullanmak yerine import ettiğimiz package üzerinden icon çağırdık. */}
               <p>Home</p>
             </div>
             <div className="item">
@@ -38,20 +100,25 @@ export default function Home() {
             {/* profile'ın konumunu kontrol etmek için wrapper içine aldık */}
             <div className="profilewrapper">
               {/* profile için ayrı bir div açtık */}
-              <div className="profile">
+              {/* component değişikliğini loading üzerinden kontrol ediyoruz.
+              structure: condition ? true: false
+              */}
+              {loading ? <p>Loading.</p> : <div className="profile">
                 {/* profile image, name ve more butonu için ayrı ayrı divler açtık */}
                 <div className="profile-image">
-                  <img src="/profile.jpeg" alt="" />
+                  {/* tanımladığımız getter'lar üzerinden verimizi çağırıyoruz. */}
+                   <img src={user.profileImage} alt="" />
                 </div>
                 <div className="profile-name">
-                  <p>Jane Doe</p>
-                  <p>@janedoe</p>
+                  <p>{user.name}</p>
+                  <p>{user.username}</p>
                 </div>
 
                 <div className="profile-more">
                     <img src="/png/more_horiz_FILL0_wght400_GRAD0_opsz24.png" alt="" />
                 </div>
               </div>
+              }
 
             </div>
 
@@ -74,6 +141,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+
 
 
   )
